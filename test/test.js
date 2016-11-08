36c5aa36bab6f22bda5824a5890bb22b92d5895b
Andrei Barbuta <andreibarbuta@yahoo.com>
2016-11-09 00:35:37 +0200

    Correct test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
-describe('This test', function() {
-  'use strict';
-  it('should always return true', function() {
-    // This would fail because true !== false
-    expect(true).toBe(false);
+describe('Add', function() {
+  it('should add two numbers together', function() {
+    // 1 + 2 should = 3
+    expect(add(1, 2)).toBe(3);
+    // 3 + 6 should = 9
+    expect(add(3, 6)).toBe(9);
   });
 });
